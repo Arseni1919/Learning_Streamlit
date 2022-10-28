63010f08ffe9ae5d23824b1a7473adb4ae585b5f
perchik_air <1919ars@gmail.com>
2022-10-28 10:59:31 +0300

    first

---
+++ README.md
@@ -0,0 +1,6 @@
+# Learning Streamlit
+
+
+## Credits
+
+- [Rum Streamlit from PyCharm](https://discuss.streamlit.io/t/version-1-5-0/21455/11)
---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+"""
+# Hello World!
+
+"""
+
+def main():
+
+    # st.snow()
+    # st.balloons()
+    st.write('# BLAAA!!!')
+    st.write('# BLAAA!!!')
+    # st.write('# BLAAA!!!')
+    # st.write('# BLAAA!!!')
+    x = 2
+    st.help(st.write)
+
+    a = 2
+
+
+if __name__ == '__main__':
+    main()
